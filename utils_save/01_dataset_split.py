8240475f827fa307463d034122347a4bbe7890b7
Longhao Zhang <zlh_ustb2022@163.com>
2026-02-22 05:04:27 +0800

    Translate comments to English in dataset_split.py
    Updated comments from Chinese to English for clarity.

--- utils_save/01_dataset_split.py
+++ utils_save/01_dataset_split.py
@@ -4,34 +4,33 @@ from sklearn.model_selection import GroupKFold
 import numpy as np
 
 def has_common_elements(list1, list2):
-    """使用集合判断两个列表是否有相同元素"""
+    """Use sets to determine whether two lists contain identical elements."""
     return bool(set(list1) & set(list2))
 
 if __name__ == '__main__':
-    # data_dir = r'zigong_data\\20251227_final_processed_data.csv' # 改1
-    # output_dir = r'data_base\\01_midim_db\\full_assessment\\6-4\\'  # 改2
-    # test_size = 0.4  # 改3
+    # data_dir = r'zigong_data\\20251227_final_processed_data.csv'
+    # output_dir = r'data_base\\01_midim_db\\full_assessment\\6-4\\'
+    # test_size = 0.4
     # df = pd.read_csv(data_dir)
     # X = df
     # y = df['consciousness']
     # target_col = 'consciousness'
-    # # 根据患者ID划分训练集和测试集，确保同一患者的数据不出现在训练集和测试集中
-    # PATIENT_ID和INP_NO是一一对应的关系，可以任选其一作为分组依据
+    # # Divide the training and test sets based on patient IDs, ensuring that data from the same patient does not appear in both the training and test sets.
+    # PATIENT_ID and INP_NO maintain a one-to-one correspondence and can be used interchangeably as the grouping basis.
     # # 7:3 ==> (1241, 532), 6:4 ==> (1063, 710)
     # unique_ids = df['INP_NO'].unique()
-    # # 获取不同患者ID对应的觉醒度标签
+    # # Retrieve the arousal level labels corresponding to different patient IDs
     # id_to_consciousness = df.groupby('INP_NO')['consciousness'].first().to_dict()
-    # # 将stratify参数设置为对应患者ID的觉醒度标签
+    # # Set the stratify parameter to the arousal level label corresponding to the patient ID.
     # train_ids, test_ids = train_test_split(
     #     unique_ids,
     #     random_state=42,
     #     stratify=[id_to_consciousness[i] for i in unique_ids],
     #     test_size=test_size
     # )
-    # # # 打印训练集和测试集的患者数量
     # print(f'Number of unique patients in training set: {len(train_ids)}')
     # print(f'Number of unique patients in testing set: {len(test_ids)}')
-    # # # 根据划分好的训练集和测试集ID，获取对应的训练集和测试集
+    # # # Based on the assigned training set and test set IDs, retrieve the corresponding training set and test set.
     # train_mask = df['INP_NO'].isin(train_ids)
     # test_mask = df['INP_NO'].isin(test_ids)
     # X_train = X[train_mask]
@@ -40,20 +39,20 @@ if __name__ == '__main__':
     # y_test = y[test_mask]
     # print(f'Training set shape: {X_train.shape}, Testing set shape: {X_test.shape}')
     # ########################################################################
-    # # 只针对训练集计算motion和language的中位数进行填充
-    # # 保留2个表变量，第一个记录motion不为-1的那些记录，第二个记录language不为-1的那些记录
+    # # Padding is performed only on the median values of motion and language calculated for the training set.
+    # # Maintain two table variables: the first records entries where motion is not -1, and the second records entries where language is not -1.
     # X_train_motion_not_minus1 = X_train[X_train['motion'] != -1]
     # X_train_language_not_minus1 = X_train[X_train['language'] != -1]
     # print(X_train_motion_not_minus1, X_train_language_not_minus1)
-    # # 统计不同consciousness下，对应的motion的中位数并打印
+    # # Calculate the median of the corresponding motion under different consciousness states and print the results.
     # motion_median = X_train_motion_not_minus1.groupby('consciousness')['motion'].median()
-    # print("不同consciousness下，motion的中位数为：")
+    # print("Under different states of consciousness, the median of motion is:")
     # print(motion_median)
-    # # 统计不同consciousness下，对应的language的中位数并打印
+    # # Calculate the median of the corresponding language under different consciousness states and print it.
     # language_median = X_train_language_not_minus1.groupby('consciousness')['language'].median()
-    # print("不同consciousness下，language的中位数为：")
+    # print("Under different states of consciousness, the median of language is:")
     # print(language_median)
-    # # 查找df中的motion为-1的记录，根据consciousness填充上边计算出的中位数motion_median，增加新的一列，标记是否填充
+    # # Find records in df where motion is -1. For each such record, fill in the previously calculated median motion value (motion_median) based on the consciousness field. Add a new column to indicate whether the value was filled.
     # for index, row in X_train.iterrows():
     #     if row['motion'] == -1:
     #         consciousness_value = row['consciousness']
@@ -61,7 +60,7 @@ if __name__ == '__main__':
     #         X_train.at[index, 'motion_filled'] = 1
     #     else:
     #         X_train.at[index, 'motion_filled'] = 0
-    # # 查找df中的language为-1的记录，根据consciousness填充上边计算出的中位数language_median
+    # # Find records in df where language is -1 and fill in the calculated language_median based on consciousness.
     # for index, row in X_train.iterrows():
     #     if row['language'] == -1:
     #         consciousness_value = row['consciousness']
@@ -69,10 +68,10 @@ if __name__ == '__main__':
     #         X_train.at[index, 'language_filled'] = 1
     #     else:
     #         X_train.at[index, 'language_filled'] = 0
-    # # 统计language和motion不同取值的数量
+    # # Count the number of distinct values for language and motion.
     # motion_counts = X_train['motion'].value_counts()
     # language_counts = X_train['language'].value_counts()
-    # # 根据对应关系处理测试集数据
+    # # Process the test set data according to the correspondence relation.
     # for index, row in X_test.iterrows():
     #     if row['motion'] == -1:
     #         consciousness_value = row['consciousness']
@@ -87,13 +86,13 @@ if __name__ == '__main__':
     #         X_test.at[index, 'language_filled'] = 1
     #     else:
     #         X_test.at[index, 'language_filled'] = 0
-    # # 保存填充关系到csv文件
+    # # Save the fill relation to the CSV file
     # motion_median.to_csv(r'zigong_data\\' + output_dir.split(r"\\")[1] + '_' + output_dir.split(r"\\")[3] +  '_motion_median_filled.csv')
     # language_median.to_csv(r'zigong_data\\' + output_dir.split(r"\\")[1] + '_' + output_dir.split(r"\\")[3] + '_language_median_filled.csv')
-    # # 保存训练集X_train和测试集X_test
+    # # Save the training set X_train and the test set X_test
     # X_train = X_train.to_csv(output_dir + 'train.csv', index=False)
     # X_test = X_test.to_csv(output_dir + 'test.csv', index=False)
-    #         # # 将处理后的数据保存到新的csv文件中
+    #         # # Save the processed data to a new CSV file.
     #         # df.to_csv(r'zigong_data\20251216_final_processed_filled.csv', index=False)
     #         # ########################################################################
     #         # train_data = pd.DataFrame(X_train, columns=X.columns)
@@ -103,38 +102,18 @@ if __name__ == '__main__':
     #         # train_data.to_csv(output_dir + 'train.csv', index=False)
     #         # test_data.to_csv(output_dir + 'test.csv', index=False)
     # exit()
-    # #############################将测试集中的gcs编码项挑选出来###############################
-    df_test = pd.read_csv(r'data_base\\01_midim_db\\full_assessment\\6-4\\test.csv')  # 改4
-    # 使用isin方法筛选
+    # #############################Select the GCS-coded items from the test set.###############################
+    df_test = pd.read_csv(r'data_base\\01_midim_db\\full_assessment\\6-4\\test.csv')
+    # Filter using the isin method
     columns_to_check = ['open_one\'s_eyes', 'motion', 'language']
     # df_gcs_encoding = df_test[df_test[columns_to_check].isin([-1]).any(axis=1)]
-    # 判断motion_filled或language_filled是否为True
+    # Determine whether motion_filled or language_filled is True
     mask_motion_filled = df_test['motion_filled'] == 1
     mask_language_filled = df_test['language_filled'] == 1
     df_gcs_encoding = df_test[mask_motion_filled | mask_language_filled]
-    df_gcs_encoding.to_csv(r'data_base\\01_midim_db\\abnormal_assessment\\6_4_test_filled.csv')  # 改5
+    df_gcs_encoding.to_csv(r'data_base\\01_midim_db\\abnormal_assessment\\6_4_test_filled.csv')
     mask_motion_unfilled = df_test['motion_filled'] == 0
     mask_language_unfilled = df_test['language_filled'] == 0
     df_gcs_encoding_unfilled = df_test[mask_motion_unfilled & mask_language_unfilled]
-    df_gcs_encoding_unfilled.to_csv(r'data_base\\01_midim_db\\normal_assessment\\6_4_test_unfilled.csv')  # 改5
+    df_gcs_encoding_unfilled.to_csv(r'data_base\\01_midim_db\\normal_assessment\\6_4_test_unfilled.csv')
     exit()
-    #############################5折交叉验证#############################
-    data_dir = 'zigong_data/final_processed_data.csv'
-    group_kfold = GroupKFold(n_splits=5)
-    df = pd.read_csv(data_dir)
-    X = df.drop(columns=['consciousness'])
-    y = df['consciousness']
-    patient_id = df['INP_NO']
-    fold_train = []
-    fold_test = []
-    for fold, (train_idx, test_idx) in enumerate(group_kfold.split(X, y, patient_id)):
-        fold_train.append(df['INP_NO'].iloc[train_idx])
-        fold_test.append(df['INP_NO'].iloc[test_idx])
-    for i, ele in enumerate(fold_train):
-        train_mask = patient_id.isin(ele)
-        df_train = df[train_mask]
-        df_train.to_csv(r'data_base\\5-folder\\' + str(i+1) + '-fold-train.csv')
-    for i, ele in enumerate(fold_test):
-        test_mask = patient_id.isin(ele)
-        df_test = df[test_mask]
-        df_test.to_csv(r'data_base\\5-folder\\' + str(i+1) + '-fold-test.csv')
