ea6aeaf1f7d501604024784278d8b5632729d8c8
Longhao Zhang <zlh_ustb2022@163.com>
2026-02-22 03:26:23 +0800

    Translate comments to English in main.py
    Updated comments in main.py to English for clarity.

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pandas as pd
 from train_zigong import train_all_models
 
 if __name__ == '__main__':
-    # 注意以下train_zigong.py中的内容
     seed = [42, 230, 4399, 9999, 55000]
     ###########################################################################
-    seed = seed[4] # 改1：选择一个种子进行实验
+    seed = seed[4] # Select a seed for experimentation
     ###########################################################################
-    df_train = pd.read_csv(r'data_base\\03_cos_similarity_db\\full_assessment\\6-4\\train.csv')  # 改2
-    df_test = pd.read_csv(r'data_base\\03_cos_similarity_db\\full_assessment\\6-4\\test.csv') # 改3
-    model_save_pth = r'model_pth\\exp_2\\02_similarity\\05_motion_language\\6-4\\seed_' + str(seed) + r'\\' # 改4
+    df_train = pd.read_csv(r'data_base\\03_cos_similarity_db\\full_assessment\\6-4\\train.csv')  
+    df_test = pd.read_csv(r'data_base\\03_cos_similarity_db\\full_assessment\\6-4\\test.csv') 
+    model_save_pth = r'model_pth\\exp_2\\02_similarity\\05_motion_language\\6-4\\seed_' + str(seed) + r'\\' 
     ###########################################################################
     target_col = 'consciousness'
     select_columns = ['INP_NO', 'Age', 'SEX', 'heart_rate', 'breathing', 'Blood_oxygen_saturation', 
@@ -24,11 +23,11 @@ if __name__ == '__main__':
     ###########################################################################
     type_con = ""
     # open_one's_eyes,motion,language,motion_filled,language_filled
-    Columns = [Columns[-4], Columns[-3], Columns[-2], Columns[-1]]  # 更换指标时修改
+    Columns = [Columns[-4], Columns[-3], Columns[-2], Columns[-1]]  # Modify when changing indicators
     continuous_cols = []
     categorical_cols = Columns
     ############################################################################
-    # 训练特征与测试特征集合
+    # Training feature set and test feature set
     X_train = X_train[Columns]
     X_test = X_test[Columns]
     # print(Columns, continuous_cols, categorical_cols)
